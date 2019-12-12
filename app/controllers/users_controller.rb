0ad80cbb5abb41ee108780441da08214d872eeef
kazuto.maeda <min6906320@gmail.com>
2019-12-12 05:47:40 +0000

    #2 #3 #4 #5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   
   def index
+    @users = User.all
   end 
   
   def new
@@ -8,29 +9,45 @@ class UsersController < ApplicationController
   end 
   
   def create
-    @user = User.new(params_user)
+    @user = User.new(user_params)
     if @user.save
-      redirect_to(root_url)
+      flash[:success] = "ユーザーの登録が完了しました"
+      redirect_to(@user)
     else
+      flash[:danger] = "ユーザーの登録に失敗しました"
       render(:new)
     end
   end 
   
   def show
+    @user = User.find(params[:id])
   end 
   
   def edit
+    @user = User.find(params[:id])
   end 
   
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "ユーザー情報を編集しました"
+      redirect_to(users_path)
+    else
+      flash[:danger] = "ユーザー情報の編集に失敗しました"
+      render(:edit)
+    end
   end 
   
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = "ユーザー情報を削除しました"
+    redirect_to(users_path)
   end 
 
  private
  
- def params_user
+ def user_params
    params.require(:user).permit(:name, :email, :password, :password_confirmation)
  end
 end
