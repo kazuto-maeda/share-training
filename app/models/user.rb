0e1b58aebfd1283f4451dce2b300a480a491b4e4
kazuto-maeda <min6906320@gmail.com>
2020-01-08 09:51:50 +0900

    forbid_negative_number_inprofile

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,22 +8,22 @@ class User < ApplicationRecord
   attr_accessor :current_password
   validates :password, presence: { if: :current_password }
   has_secure_password
-  
+
   has_many :tweets, dependent: :destroy
-  
+
   has_many :relationships, dependent: :destroy
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy
   has_many :followers, through: :reverses_of_relationship, source: :user
-  
+
   has_many :favorites, dependent: :destroy
   has_many :favorite_tweets, through: :favorites, source: :tweet
-  
+
   has_many :comments, dependent: :destroy
   has_many :comment_tweets, through: :comments, source: :tweet
-  
+
   has_many :alerts, dependent: :destroy
-  
+
   def follow(other_user)
     if self != other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -31,35 +31,35 @@ class User < ApplicationRecord
       other_user.alerts.create(relationship_id: relationship.id)
     end
   end
-  
+
   def unfollow(other_user)
     relationship = self.relationships.find_by(follow_id: other_user.id)
     relationship.destroy if relationship
   end
-  
+
   def following?(other_user)
     self.followings.include?(other_user)
   end
-  
+
   def feed_tweets
     Tweet.where(user_id: self.following_ids + [self.id])
   end
-  
+
   def favorite(tweet)
       self.favorites.find_or_create_by(tweet_id: tweet.id)
       if self.id != tweet.user.id
         favorite = self.favorites.find_by(tweet_id: tweet.id)
         tweet.user.alerts.create(favorite_id: favorite.id)
       end
-  end 
-  
+  end
+
   def quit_favorite(tweet)
     favorite = self.favorites.find_by(tweet_id: tweet.id)
     favorite.destroy if favorite
-  end 
-  
+  end
+
   def favorite?(tweet)
     self.favorite_tweets.include?(tweet)
-  end 
-  
+  end
+
 end
